83ae13d7c4e6a0324dd04e187a609f1461f02daa
Motasim <44056349+motasimmakki@users.noreply.github.com>
2020-08-18 21:19:56 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React,{ Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import Header from './Header';
+import Body from './Body';
+import Footer from './Footer';
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <Navbar />
+        <Header />
+        <Body />
+        <hr />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
